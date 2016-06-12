462e88a62490d237759a357ede9537d40621fee8
Administrator <Administrator@192.168.1.104>
2016-06-12 16:03:27 +0800

    entity

---
+++ src/com/htjx/user/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.htjx.user.controller;
+
+import com.htjx.user.entity.User;
+
+public class UserController {
+
+	public static void main(String[] args) {
+		
+		User user=new User();
+		user.setAge(12);
+		user.setId(1);
+		user.setName("yang");
+		
+		System.out.println(user);
+	}
+}
