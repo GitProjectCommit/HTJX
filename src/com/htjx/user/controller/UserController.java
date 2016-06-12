46ebd5a97e3eede8090e67c4ee6803f262e7c493
GitProjectCommit <2690164089@qq.com>
2016-06-12 16:43:30 +0800

    009001

--- src/com/htjx/user/controller/UserController.java
+++ src/com/htjx/user/controller/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
 		User user=new User();
 		user.setAge(12);
 		user.setId(1);
-		user.setName("yang006");
+		user.setName("yang006009001");
 		
 		System.out.println(user);
 	}
